a805062ab3c98ffa6e92cb18c40d6ce284c47576
Alejandro <alejandrofdom@gmail.com>
2025-03-23 17:05:38 +0100

    feat: update app

--- audio_transcript.py
+++ audio_transcript.py
@@ -6,17 +6,18 @@ import whisper
 def audio_transcript(video_url):
     transcript_text = ""
     ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': 'video_audio.%(ext)s',
+        "format": "bestaudio/best",
+        "outtmpl": "video_audio.%(ext)s",
         # 'ffmpeg_location': '/usr/local/bin/ffmpeg',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }
+        ],
     }
 
-    # Download audio using yt-dlp
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([video_url])
@@ -24,7 +25,6 @@ def audio_transcript(video_url):
     except Exception as e:
         print(f"An error occurred while downloading audio: {e}")
 
-    # Use the downloaded audio file for transcription
     audio_file = "video_audio.mp3"
     try:
         model = whisper.load_model("base")
@@ -35,9 +35,6 @@ def audio_transcript(video_url):
     except Exception as e:
         print(f"An error occurred during transcription: {e}")
 
-    # Clean up the audio file
     if os.path.exists(audio_file):
         os.remove(audio_file)
     return transcript_text
-
-
